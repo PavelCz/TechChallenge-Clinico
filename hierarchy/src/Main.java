47a9f4822264858dea1684ce2dcf02b6f8f4eab8
Pavel Czempin <valep.pave@gmx.de>
2020-01-07 18:38:27 +0100

    Implement function for getting all questions for a given language
    Print all questions in the nurse's language

--- hierarchy/src/Main.java
+++ hierarchy/src/Main.java
@@ -1,27 +1,45 @@
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void run() {
+    private static List<String> getAllQuestionsForLanguage(JsonArray questions, String language) {
+        List<String> nurseQuestions = new ArrayList<>();
+        questions.forEach(q ->
+                nurseQuestions.add(
+                        q.getAsJsonObject().get("translations").getAsJsonObject().get(language).getAsJsonObject()
+                                .get("question").getAsString()
+                )
+        );
+        return nurseQuestions;
+    }
+
+    private static void run() {
         String translationsPath = "data/translations.json";
 
         String nurseLanguage = "german";
 
+        // Open the file and serialize the data into a json object
         FileReader fr = null;
-        try {
-            fr = new FileReader(translationsPath);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        while (fr == null) {
+            try {
+                fr = new FileReader(translationsPath);
+            } catch (FileNotFoundException e) {
+                // Pass, retry the while loop
+            }
         }
+        JsonObject translation = JsonParser.parseReader(fr).getAsJsonObject();
+        JsonArray questions = translation.get("abdominal").getAsJsonArray();
 
-        JsonObject jsonObject = JsonParser.parseReader(fr).getAsJsonObject();
 
-        System.out.println(jsonObject.get("abdominal").getAsJsonArray().get(0).getAsJsonObject().get("translations")
-                .getAsJsonObject().get(nurseLanguage).getAsJsonObject());
+        List<String> nurseQuestions = getAllQuestionsForLanguage(questions, nurseLanguage);
+        System.out.println(nurseQuestions);
 
 
     }
