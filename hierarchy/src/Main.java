b975d8b6038b3af8da4272d83d01311ab2c4d946
Pavel Czempin <valep.pave@gmx.de>
2020-01-07 18:22:49 +0100

    Add json parsing example

--- hierarchy/src/Main.java
+++ hierarchy/src/Main.java
@@ -1,3 +1,32 @@
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
 
+    public static void run() {
+        String translationsPath = "data/translations.json";
+
+        String nurseLanguage = "german";
+
+        FileReader fr = null;
+        try {
+            fr = new FileReader(translationsPath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        JsonObject jsonObject = JsonParser.parseReader(fr).getAsJsonObject();
+
+        System.out.println(jsonObject.get("abdominal").getAsJsonArray().get(0).getAsJsonObject().get("translations")
+                .getAsJsonObject().get(nurseLanguage).getAsJsonObject());
+
+
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
 }
