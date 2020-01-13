1ff9db05dc32b0d85f904aff8ae44d8eb0217486
Pavel Czempin <valep.pave@gmx.de>
2020-01-13 15:32:23 +0100

    Fix node and update gitignore

--- NODE/models/index.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI ||
-    "mongodb://localhost:27017/booksData",{ useUnifiedTopology: true, useNewUrlParser: true });
-
-module.exports.books = require("./books.js");
